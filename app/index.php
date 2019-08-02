78ff818ea147191631b5b7df6dbb6a5586da643c
FloorJulesSegers <floorsegers70@gmail.com>
2019-08-02 14:52:54 +0200

    remove section from form

--- app/index.php
+++ app/index.php
@@ -88,13 +88,7 @@ if (!$errfirstName && !$errlastName && !$errEmail && !$errMessage) {
 							<textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
 							<?php echo "<p class='text-danger'>$errMessage</p>";?>
 						</div>
-					</div>
-					<!--<div class="form-group">
-						<label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
-						<div class="col-sm-10">
-							<input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
-						</div>
-					</div>-->
+					</div>>
 					<div class="form-group">
 						<div class="col-sm-10 col-sm-offset-2">
 							<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
